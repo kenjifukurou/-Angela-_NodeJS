481cfdee092fb77dc4ef5b0217fa0f968ca17ecb
kenjifukurou <kenjifukurou@gmail.com>
2021-05-02 19:17:16 +0800

    node js study

---
+++ main.js
@@ -0,0 +1,9 @@
+//jshint esversion:6
+
+// const fs = require('fs');
+// fs.copyFileSync("index.html", "about.html");
+
+const superheroes = require('superheroes');
+
+var hero = superheroes.random();
+console.log(hero);
